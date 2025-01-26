95d96733fd7286da1f39b9b3a491f1ed2d4e2970
Matheus Alves <matheus.alves.stela@gmail.com>
2025-01-26 17:59:36 -0300

    adicionadas options úteis do gráfico

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -1,15 +1,16 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
 
 export default function History() {
-  const [clanTag, setClanTag] = useState("2J8JP9RJR");
+  const [clanTag, setClanTag] = useState("#2J8JP9RJR");
   const [warLog, setWarLog] = useState(null);
   const [error, setError] = useState(null);
 
   const fetchWarLog = async () => {
-    const url = `http://localhost:4000/clashofclans/${encodeURIComponent(clanTag)}/warlog`;
+    const cleanedTag = clanTag.startsWith("#") ? clanTag.slice(1) : clanTag;
+    const url = `http://localhost:4000/clashofclans/${encodeURIComponent(cleanedTag)}/warlog`;
 
     try {
       const response = await fetch(url);
@@ -23,10 +24,6 @@ export default function History() {
       setError(error.message);
     }
   };
-
-  useEffect(() => {
-    fetchWarLog();
-  }, []);
 
   const data = {
     labels: warLog ? warLog.map(war => formatDate(war.endTime)) : [],
@@ -68,7 +65,7 @@ export default function History() {
 
   function formatDate(dateString) {
     if (!dateString) return "Data inválida";
-  
+
     // Extrair partes da data e hora do formato ISO 8601
     const year = dateString.slice(0, 4);
     const month = dateString.slice(4, 6);
@@ -76,34 +73,34 @@ export default function History() {
     const hour = dateString.slice(9, 11); // Ignorando o "T"
     const minutes = dateString.slice(11, 13);
     const seconds = dateString.slice(13, 15);
-  
+
     // Criar um objeto Date usando as partes extraídas
     const date = new Date(`${year}-${month}-${day}T${hour}:${minutes}:${seconds}Z`);
     if (isNaN(date.getTime())) return "Data inválida";
-  
+
     // Converter a data para o horário de Brasília (UTC-3)
     const brtOffset = -3;
     date.setHours(date.getUTCHours() + brtOffset);
-  
+
     const formattedDay = date.getDate().toString().padStart(2, "0");
     const formattedMonth = (date.getMonth() + 1).toString().padStart(2, "0"); // Janeiro é 0!
     const formattedYear = date.getFullYear();
     const formattedHours = date.getHours().toString().padStart(2, "0");
     const formattedMinutes = date.getMinutes().toString().padStart(2, "0");
-  
+
     return `${formattedDay}/${formattedMonth}/${formattedYear} - ${formattedHours}:${formattedMinutes}`;
   }
-  
+
   const options = {
     responsive: true,
     maintainAspectRatio: false,
     scales: {
-        x: {
-          reverse: true,  
-        },
-        y: {
-          beginAtZero: true,
-        },
+      x: {
+        reverse: true,  
+      },
+      y: {
+        beginAtZero: true,
+      },
     },  
   };
 
@@ -112,6 +109,7 @@ export default function History() {
       <h1 className="text-lg font-bold">História de Guerra</h1>
       <br />
       <div className="flex flex-col items-center space-y-2">
+        <p className="text-gray-400">Insira o ID do Clã:</p>
         <input
           className="h-8 rounded-full px-3 text-black font-bold bg-white"
           value={clanTag}
@@ -121,7 +119,7 @@ export default function History() {
           onClick={fetchWarLog}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
         >
-          Buscar Histórico de Guerra
+          buscar
         </button>
       </div>
       <br />
